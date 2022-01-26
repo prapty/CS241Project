f52cd91853d9757048f835a1351c90fe4ec680b2
elina <elina.vankempen@gmail.com>
2022-01-25 22:32:16 -0800

    added methods in parser

--- src/Parser.java
+++ src/Parser.java
@@ -16,17 +16,122 @@ public class Parser {
     }
 
     public void Computation(IntermediateTree irTree) throws SyntaxException, IOException {
-        /*order:
-        main, varDecl, funcDecl,"{", statsequence, "}", "."
-         */
+        //order:
+        // main, varDecl, funcDecl,"{", statsequence, "}", "."
         if (token.kind != TokenKind.reservedSymbol || token.id != ReservedWords.mainDefaultId.ordinal()) {
-            //return error
+            //if not main, return error
         } else {
             token = lexer.nextToken();
         }
         if (token.kind == TokenKind.reservedWord && token.id == ReservedWords.varDefaultId.ordinal()) {
-
+            //parse all variables
+            varDecl(irTree);
+            while (token.kind == TokenKind.reservedWord && (token.id == ReservedWords.varDefaultId.ordinal() || token.id == ReservedWords.arrayDefaultId.ordinal())) {
+                varDecl(irTree);
+            }
         }
+        //funcdecl
+        if (token.kind == TokenKind.reservedSymbol && token.id == ReservedWords.startingCurlyBracketDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            statSequence(irTree); //parse statement sequence
+        } else {
+            //if no {, error
+        }
+        if (token.kind != TokenKind.reservedSymbol || token.id != ReservedWords.endingCurlyBracketDefaultId.ordinal()) {
+            //if no }, error
+        }
+        if (token.kind != TokenKind.reservedSymbol || token.id != ReservedWords.dotDefaultId.ordinal()) {
+            //if no ., error
+        }
+    }
+
+    private void statSequence(IntermediateTree irTree) throws SyntaxException, IOException {
+        statement(irTree);
+        while (token.kind == TokenKind.reservedSymbol && token.id == ReservedWords.semicolonDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            if (token.kind != TokenKind.reservedSymbol || token.id != ReservedWords.endingCurlyBracketDefaultId.ordinal()) {
+                break; //if ; was the last optional terminating semicolon, stop parsing statements
+            }
+            statement(irTree);
+        }
+    }
+
+    private void statement(IntermediateTree irTree) throws SyntaxException, IOException {
+        //possibilities: let, call, if, while, return
+        if (token.kind != TokenKind.reservedWord) {
+            //throw error
+        }
+        if (token.id == ReservedWords.letDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            assignment(irTree);
+        }
+        if (token.id == ReservedWords.callDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            funcCall(irTree);
+        }
+        if (token.id == ReservedWords.ifDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            IfStatement(irTree);
+        }
+        if (token.id == ReservedWords.whileDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            whileStatement(irTree);
+        }
+        if (token.id == ReservedWords.returnDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            returnStatement(irTree);
+        } else {
+            //trhow error
+        }
+    }
+
+    private void assignment(IntermediateTree irTree) {
+
+    }
+
+    private void funcCall(IntermediateTree irTree) { //do later
+    }
+
+    private void IfStatement(IntermediateTree irTree) throws SyntaxException, IOException {
+        relation(irTree);
+        if (token.kind == TokenKind.reservedWord && token.id == ReservedWords.thenDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            statSequence(irTree);
+        } else {
+            //error
+        }
+        if (token.kind == TokenKind.reservedWord && token.id == ReservedWords.elseDefaultId.ordinal()) {
+            token = lexer.nextToken();
+            statSequence(irTree);
+        }
+        if (token.kind == TokenKind.reservedWord && token.id == ReservedWords.fiDefaultId.ordinal()) {
+            token = lexer.nextToken();
+        } else {
+            //error
+        }
+    }
+
+    private void whileStatement(IntermediateTree irTree) throws SyntaxException, IOException {
+        relation(irTree);
+    }
+
+
+    private void returnStatement(IntermediateTree irTree) {
+
+    }
+
+    private void relation(IntermediateTree irTree) throws SyntaxException, IOException {
+        Expression(irTree);
+        if (token.kind != TokenKind.relOp || token.id == ReservedWords.assignmentSymbolDefaultId.ordinal()) {
+            //errror
+        } else {
+            //handle relOp
+            token = lexer.nextToken();
+            Expression(irTree);
+        }
+    }
+
+    private void varDecl(IntermediateTree irTree) {
 
     }
 
@@ -51,17 +156,17 @@ public class Parser {
             token = lexer.nextToken();
             Expression(irTree);
             if (token.kind == TokenKind.reservedSymbol && token.id == ReservedWords.endingFirstBracketDefaultId.ordinal()) {
-                //something
+                //end expression
                 token = lexer.nextToken();
             } else {
                 String errorMessage = String.format(ErrorInfo.UNEXPECTED_TOKEN_PARSER_ERROR);
                 throw new SyntaxException(errorMessage);
             }
         } else if (token.kind == TokenKind.identity) {
-            //something
+            //num
             token = lexer.nextToken();
         } else if (token.kind == TokenKind.number) {
-            //something
+            //num
             token = lexer.nextToken();
         } else if (token.kind == TokenKind.reservedWord && token.id == ReservedWords.callDefaultId.ordinal()) {
             //do later
@@ -69,10 +174,4 @@ public class Parser {
     }
 
 
-    private void IfStatement(IntermediateTree irTree) throws SyntaxException, IOException {
-        Expression(irTree);
-        if (token.kind == TokenKind.relOp) {
-
-        }
-    }
 }
